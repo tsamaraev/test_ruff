077d9e2942bfea245cceec901616f0094fa57edb
user9925 <tsamaraevv@mail.ru>
2024-07-20 12:32:44 +0300

    Добавлена логика приложения

--- final_win.py
+++ final_win.py
@@ -8,18 +8,112 @@ from PyQt5.QtWidgets import (
 from instr import *
 
 class FinalWin(QWidget):
-    def __init__(self):
+    def __init__(self, exp):
         super().__init__()
+
+        self.exp = exp
 
         self.initUI()
 
         self.set_appear()
 
+
         self.show()
 
+    def results(self):
+        if self.exp.age < 7:
+            self.index = 0
+            return "Данные для этого возроста отсутствуют"
+        self.index = (4 * (int(self.exp.t1) + int(self.exp.t2) + int(self.exp.t3)) - 200) / 10
+        # для возраста 7 - 8 лет
+        if self.exp.age == 7 or self.exp.age == 8:
+            if self.index >= 21:
+                return txt_res1 
+            
+            elif 21 > self.index >= 17:
+                return txt_res2
+
+            elif 17 > self.index >= 12:
+                return txt_res3
+
+            elif 12 > self.index >= 6.5:
+                return txt_res4
+            
+            else:
+                return txt_res5
+            
+        # для возраста 9 - 10 лет
+        if self.exp.age == 9 or self.exp.age == 10:
+            if self.index >= 19.5:
+                return txt_res1 
+            
+            elif 19.5 > self.index >= 15.5:
+                return txt_res2
+
+            elif 15.5 > self.index >= 10.5:
+                return txt_res3
+
+            elif 10.5 > self.index >= 5:
+                return txt_res4
+            
+            else:
+                return txt_res5
+        
+        # для возраста 11 - 12 лет
+        if self.exp.age == 9 or self.exp.age == 10:
+            if self.index >= 18:
+                return txt_res1 
+            
+            elif 18 > self.index >= 14:
+                return txt_res2
+
+            elif 14 > self.index >= 9:
+                return txt_res3
+
+            elif 9 > self.index >= 3.5:
+                return txt_res4
+            
+            else:
+                return txt_res5
+        
+        # для возраста 13 - 14 лет
+        if self.exp.age == 9 or self.exp.age == 10:
+            if self.index >= 16.5:
+                return txt_res1 
+            
+            elif 16.5 > self.index >= 12.5:
+                return txt_res2
+
+            elif 12.5 > self.index >= 7.5:
+                return txt_res3
+
+            elif 7.5 > self.index >= 2:
+                return txt_res4
+            
+            else:
+                return txt_res5
+        
+        # для возраста от 15
+        if self.exp.age >= 15:
+            if self.index >= 15:
+                return txt_res1 
+            
+            elif 15 > self.index >= 11:
+                return txt_res2
+
+            elif 11 > self.index >= 6:
+                return txt_res3
+
+            elif 6 > self.index >= 0.5:
+                return txt_res4
+            
+            else:
+                return txt_res5
+
+
     def initUI(self):
-        self.workh_text = QLabel(txt_workheart)
-        self.index_txt = QLabel(txt_index)
+        self.workh_text = QLabel(txt_workheart + self.results())
+        self.index_txt = QLabel(txt_index + str(self.index))
 
         self.layout_line = QVBoxLayout()
 
--- second_win.py
+++ second_win.py
@@ -1,4 +1,5 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, QTime
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import (
     QApplication, QWidget,
     QHBoxLayout, QVBoxLayout,
@@ -8,7 +9,16 @@ from PyQt5.QtWidgets import (
 from instr import *
 from final_win import *
 
+
+class Expirement():
+    def __init__(self, age, test1, test2, test3):
+        self.age = age
+        self.t1 = test1
+        self.t2 = test2
+        self.t3 = test3
+
 class TestWin(QWidget):
+
     def __init__(self):
         super().__init__()
 
@@ -22,15 +32,80 @@ class TestWin(QWidget):
     
     def next_click(self):
         self.hide()
-        self.fw = FinalWin()
+        self.exp = Expirement(int(self.line_age.text()), self.line_test1.text(), self.line_test2.text(), self.line_test3.text())
+        self.fw = FinalWin(self.exp)
+
+
+    def timer_test(self):
+        global time
+        time = QTime(0, 0, 15)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer1Event)
+        self.timer.start(1000)
+
+
+    def timer_sits(self):
+      global time
+      time = QTime(0, 0, 30)
+      self.timer = QTimer()
+      self.timer.timeout.connect(self.timer2Event)
+      #одно приседание в 1.5 секунды
+      self.timer.start(1500)
+
+    def timer_final(self):
+        global time
+        time = QTime(0, 1, 0)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer3Event)
+        self.timer.start(1000)
+
+    def timer1Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss"))
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+
+    def timer2Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss")[6:8])
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+    
+
+    def timer3Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss")[6:8])
+        if int(time.toString("hh:mm:ss")[6:8]) >= 45:
+            self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        elif int(time.toString("hh:mm:ss")[6:8]) <= 15:
+            self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        else:
+            self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+
+
 
     def connetcs(self):
         self.btn_next.clicked.connect(self.next_click)
+        self.btn_test1.clicked.connect(self.timer_test)
+        self.btn_test2.clicked.connect(self.timer_sits)
+        self.btn_test3.clicked.connect(self.timer_final)
+
 
     def set_appear(self):
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
+
 
     def initUI(self):
 
